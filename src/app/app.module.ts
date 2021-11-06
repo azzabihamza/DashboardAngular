9ebdb2a6cc123508d09a9576139a64af81331b3f
azzabihamza <hamza.azzabi@esprit.tn>
2021-11-06 16:11:39 +0100

    Add shared components and dashboard & config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { TopbarComponent } from './components/shared/topbar/topbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { IonicModule } from '@ionic/angular';
+import { MainPageComponent } from './components/shared/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TopbarComponent,
+    DashboardComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

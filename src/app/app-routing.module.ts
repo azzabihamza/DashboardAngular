ae99810dcf4f24bfc5c09335a0d65bca601b8349
azzabihamza <hamza.azzabi@esprit.tn>
2021-11-12 20:40:48 +0100

    Squashed commit of the following:
    commit 9995d6b07f2e644c3044c9bcaa9ea7d2e3992aef
    Author: azzabihamza <hamza.azzabi@esprit.tn>
    Date:   Fri Nov 12 20:35:10 2021 +0100
    
        logo update
    
    commit 5f493586018ec00ee34988534e1f326d83e2fdf3
    Author: azzabihamza <hamza.azzabi@esprit.tn>
    Date:   Fri Nov 12 13:46:12 2021 +0100
    
        add routing to navigation
    
    commit dbb2bb2d71466e0495c33ce1935a0ee56094845e
    Author: azzabihamza <hamza.azzabi@esprit.tn>
    Date:   Fri Nov 12 13:06:47 2021 +0100
    
        add all main components
    
    commit 5bb618d0c4bf4345df8e9273f5c845829b0c91b0
    Author: azzabihamza <hamza.azzabi@esprit.tn>
    Date:   Fri Nov 12 10:47:24 2021 +0100
    
        Re-Order components & add element to the nav
    
    commit 9ebdb2a6cc123508d09a9576139a64af81331b3f
    Author: azzabihamza <hamza.azzabi@esprit.tn>
    Date:   Sat Nov 6 16:11:39 2021 +0100
    
        Add shared components and dashboard & config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MainCustomersComponent } from './components/main-customers/main-customers.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { MainProductsComponent } from './components/main-products/main-products.component';
+import { MainShopComponent } from './components/main-shop/main-shop.component';
+import { MainStockComponent } from './components/main-stock/main-stock.component';
+import { MainProvidersComponent } from './components/main-providers/main-providers.component';
+import { MainInvoicesComponent } from './components/main-invoices/main-invoices.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainPageComponent, children: [
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'customers', component: MainCustomersComponent},
+    {path: 'products', component: MainProductsComponent},
+    {path: 'stock', component: MainStockComponent},
+    {path: 'shop', component: MainShopComponent},
+    {path: 'providers', component: MainProvidersComponent},
+    {path: 'invoices', component: MainInvoicesComponent},
+    {path: '', redirectTo: 'dashboard', pathMatch: 'full'}
+  ]},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { TopbarComponent } from './components/shared/topbar/topbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { IonicModule } from '@ionic/angular';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainCustomersComponent } from './components/main-customers/main-customers.component';
+import { MainProductsComponent } from './components/main-products/main-products.component';
+import { MainStockComponent } from './components/main-stock/main-stock.component';
+import { MainShopComponent } from './components/main-shop/main-shop.component';
+import { MainProvidersComponent } from './components/main-providers/main-providers.component';
+import { MainInvoicesComponent } from './components/main-invoices/main-invoices.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TopbarComponent,
+    DashboardComponent,
+    MainPageComponent,
+    MainCustomersComponent,
+    MainProductsComponent,
+    MainStockComponent,
+    MainShopComponent,
+    MainProvidersComponent,
+    MainInvoicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot(),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
